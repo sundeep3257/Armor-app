877c1104707a314298aa34df630269a41916444f
sundeep3257 <119443659+sundeep3257@users.noreply.github.com>
2024-10-04 19:28:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -171,5 +171,5 @@ def download_csv():
     return send_from_directory(directory=directory, path=csv_filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    #app.run()
+    #app.run(debug=True)
+    app.run()
